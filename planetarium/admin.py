db3b39df3f0e06a03f766403e4e025022b89d0c1
Anastasiia Mishchuk <mishchuk.anastasiia@gmail.com>
2024-11-16 18:07:22 +0200

    registered Reservation & Ticket models in admin page

--- planetarium/admin.py
+++ planetarium/admin.py
@@ -4,11 +4,15 @@ from .models import (
     AstronomyShow,
     ShowTheme,
     PlanetariumDome,
-    ShowSession
+    ShowSession,
+    Reservation,
+    Ticket
 )
 
 
-admin.site.register(AstronomyShow)
 admin.site.register(ShowTheme)
 admin.site.register(PlanetariumDome)
+admin.site.register(AstronomyShow)
 admin.site.register(ShowSession)
+admin.site.register(Reservation)
+admin.site.register(Ticket)
