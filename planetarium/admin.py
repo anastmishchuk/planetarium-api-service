2e385c6da73fd5723de2f463f185cce0a531cf0d
Anastasiia Mishchuk <mishchuk.anastasiia@gmail.com>
2024-11-16 19:07:08 +0200

    add Ticket inline to ReservationAdmin in Django admin panel

--- planetarium/admin.py
+++ planetarium/admin.py
@@ -10,6 +10,16 @@ from .models import (
 )
 
 
+class TicketInLine(admin.TabularInline):
+    model = Ticket
+    extra = 1
+
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    inlines = (TicketInLine,)
+
+
 admin.site.register(ShowTheme)
 admin.site.register(PlanetariumDome)
 admin.site.register(AstronomyShow)
