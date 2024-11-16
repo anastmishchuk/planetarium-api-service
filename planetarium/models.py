921a1237fffeaf41b51bbe24e496f7a95a774f4a
Anastasiia Mishchuk <mishchuk.anastasiia@gmail.com>
2024-11-16 15:20:24 +0200

    created models for api: AstronomyShow, ShowTheme, PlanetariumDome, ShowSession

--- planetarium/models.py
+++ planetarium/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+
+class AstronomyShow(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+
+    class Meta:
+        ordering = ["title"]
+
+    def __str__(self):
+        return self.title
+
+
+class ShowTheme(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class PlanetariumDome(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+    @property
+    def capacity(self) -> int:
+        return self.rows * self.seats_in_row
+
+    def __str__(self):
+        return self.name
+
+
+class ShowSession(models.Model):
+    astronomy_show = models.ForeignKey(AstronomyShow, on_delete=models.CASCADE)
+    planetarium_dome = models.ForeignKey(PlanetariumDome, on_delete=models.CASCADE)
+    show_time = models.DateTimeField()
+
+    class Meta:
+        ordering = ["-show_time"]
+
+    def __str__(self):
+        return self.astronomy_show.title + " " + str(self.show_time)
