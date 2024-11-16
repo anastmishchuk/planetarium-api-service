213b207e6ff5e51cdd18ef21de77103159d593f3
Anastasiia Mishchuk <mishchuk.anastasiia@gmail.com>
2024-11-16 16:10:43 +0200

    reorganized order of models

--- planetarium/models.py
+++ planetarium/models.py
@@ -1,17 +1,6 @@
 from django.db import models
 
 
-class AstronomyShow(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.TextField()
-
-    class Meta:
-        ordering = ["title"]
-
-    def __str__(self):
-        return self.title
-
-
 class ShowTheme(models.Model):
     name = models.CharField(max_length=255)
 
@@ -30,6 +19,17 @@ class PlanetariumDome(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class AstronomyShow(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+
+    class Meta:
+        ordering = ["title"]
+
+    def __str__(self):
+        return self.title
 
 
 class ShowSession(models.Model):
