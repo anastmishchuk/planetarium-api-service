f7961b15c04d520bd03c46621b5ddfd494265ea4
Anastasiia Mishchuk <mishchuk.anastasiia@gmail.com>
2024-11-18 12:53:49 +0200

    add debug toolbar to middleware in settings

--- planetarium_api_service/settings.py
+++ planetarium_api_service/settings.py
@@ -47,6 +47,7 @@ AUTH_USER_MODEL = "user.User"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
