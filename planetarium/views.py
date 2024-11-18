ef1a2a375ed95dff14ade4a3e0367fb640a47dae
Anastasiia Mishchuk <mishchuk.anastasiia@gmail.com>
2024-11-18 12:11:07 +0200

    add custom get_queryset & get_serializer_class for ShowSessionViewSet

--- planetarium/views.py
+++ planetarium/views.py
@@ -15,7 +15,7 @@ from planetarium.serializers import (
     ShowSessionSerializer,
     ReservationSerializer,
     AstronomyShowListSerializer,
-    AstronomyShowRetrieveSerializer
+    AstronomyShowRetrieveSerializer, ShowSessionListSerializer, ShowSessionRetrieveSerializer
 )
 
 
@@ -50,6 +50,19 @@ class AstronomyShowViewSet(viewsets.ModelViewSet):
 class ShowSessionViewSet(viewsets.ModelViewSet):
     queryset = ShowSession.objects.all()
     serializer_class = ShowSessionSerializer
+
+    def get_queryset(self):
+        queryset = self.queryset
+        if self.action in ("list", "retrieve"):
+            return queryset.select_related("astronomy_show", "planetarium_dome")
+        return queryset
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return ShowSessionListSerializer
+        if self.action == "retrieve":
+            return ShowSessionRetrieveSerializer
+        return ShowSessionSerializer
 
 
 class ReservationViewSet(viewsets.ModelViewSet):
