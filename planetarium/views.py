4f6dc4a8609e88f1c52ad8824a6f95eebcaf53c8
Anastasiia Mishchuk <mishchuk.anastasiia@gmail.com>
2024-11-18 12:35:11 +0200

    add filtering methods to AstronomyShowViewSet for title and show themes

--- planetarium/views.py
+++ planetarium/views.py
@@ -12,10 +12,12 @@ from planetarium.serializers import (
     ShowThemeSerializer,
     PlanetariumDomeSerializer,
     AstronomyShowSerializer,
-    ShowSessionSerializer,
-    ReservationSerializer,
     AstronomyShowListSerializer,
-    AstronomyShowRetrieveSerializer, ShowSessionListSerializer, ShowSessionRetrieveSerializer
+    AstronomyShowRetrieveSerializer,
+    ShowSessionSerializer,
+    ShowSessionListSerializer,
+    ShowSessionRetrieveSerializer,
+    ReservationSerializer,
 )
 
 
@@ -30,14 +32,24 @@ class PlanetariumDomeViewSet(viewsets.ModelViewSet):
 
 
 class AstronomyShowViewSet(viewsets.ModelViewSet):
-    queryset = AstronomyShow.objects.all()
+    queryset = AstronomyShow.objects.prefetch_related("show_themes")
     serializer_class = AstronomyShowSerializer
+
+    @staticmethod
+    def _params_to_ints(query_string):
+        return [int(str_id) for str_id in query_string.split(",")]
 
     def get_queryset(self):
         queryset = self.queryset
-        if self.action in ("list", "retrieve"):
-            queryset = queryset.prefetch_related("show_themes")
-        return queryset
+        title = self.request.query_params.get("title")
+        show_themes = self.request.query_params.get("show_themes")
+
+        if title:
+            queryset = queryset.filter(title__icontains=title)
+        if show_themes:
+            show_themes = self._params_to_ints(show_themes)
+            queryset = queryset.filter(show_themes__id__in=show_themes)
+        return queryset.distinct()
 
     def get_serializer_class(self):
         if self.action == "list":
@@ -48,7 +60,9 @@ class AstronomyShowViewSet(viewsets.ModelViewSet):
 
 
 class ShowSessionViewSet(viewsets.ModelViewSet):
-    queryset = ShowSession.objects.all()
+    queryset = ShowSession.objects.all().select_related(
+        "astronomy_show", "planetarium_dome"
+    )
     serializer_class = ShowSessionSerializer
 
     def get_queryset(self):
