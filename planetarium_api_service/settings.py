3211e94762c9d21d0167fa70604fde6e84b08a9f
Anastasiia Mishchuk <mishchuk.anastasiia@gmail.com>
2024-11-19 14:30:37 +0200

    add throttle settings to rest_framework

--- planetarium_api_service/settings.py
+++ planetarium_api_service/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "rest_framework.authtoken",
+    "drf_spectacular",
     "debug_toolbar",
     "planetarium",
     "user",
@@ -142,5 +143,15 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.AllowAny",],
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication",
-    ]
+    ],
+    "DEFAULT_SCHEMA_CLASS":
+        "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle"
+    ],
+    "DEFAULT_THROTTLE_RATES": {
+        "anon": "20/minute",
+        "user": "40/minute"
+    }
 }
